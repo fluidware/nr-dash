c6fc3e4ef50a380c82a2debbe31148ab09c0f65f
Jason <jmulligan@fluidware.com>
2014-06-28 13:15:13 -0400

    Forgot to add this file

---
+++ src/cycle.js
@@ -0,0 +1,17 @@
+/**
+ * Cycles the nav 'pills'
+ *
+ * @method cycle
+ * @param  {Number} secs Number of seconds to pause while cycling
+ * @return {Undefined}   undefined
+ */
+function cycle ( secs ) {
+	repeat( function () {
+		var current = $( "ul.pills .active")[0],
+		    next    = current.nextElementSibling || current.parentNode.childNodes[0];
+
+		if ( !element.hasClass( next, "active") ) {
+			element.dispatch( next.childNodes[0], "click" );
+		}
+	}, secs * 1000, "navCycle", false );
+}
